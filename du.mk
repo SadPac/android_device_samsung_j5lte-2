9ca6a762fc6094bf3fe4c4165552f4144b1abeb4
SadPac <llok.hr@gmail.com>
2017-12-13 02:51:04 -0500

    Update du.mk

--- du.mk
+++ du.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/samsung/j5lte/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5lte
-PRODUCT_NAME := slim_j5lte
+PRODUCT_NAME := du_j5lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J500F
 PRODUCT_MANUFACTURER := samsung
