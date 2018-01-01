a719656ab84d26e9b5848e87207d6dba6f889942
SadPac <llok.hr@gmail.com>
2018-01-01 13:13:53 -0500

    Update and rename slim.mk to dot.mk

--- dot.mk
+++ dot.mk
@@ -1,6 +1,6 @@
 
 # Inherit from common
-$(call inherit-product, device/samsung/j5-common/slim.mk)
+$(call inherit-product, device/samsung/j5-common/dot.mk)
 
 $(call inherit-product, device/samsung/j5lte/device.mk)
 
